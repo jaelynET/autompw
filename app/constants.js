13db52679addd236ccbb35981b02470cf007716e
Marlon Thomas <jlyn.thomas.jt@gmail.com>
2026-07-29 20:06:38 -0700

    Added installation note

--- app/constants.js
+++ app/constants.js
@@ -77,7 +77,8 @@ export const product = {
       "Chevrolet Silverado 2500",
       "Chevrolet Avalanche (without factory cladding)",
     ],
-    installationNote: "Fits Avalanche models without factory cladding only.",
+    installationNote:
+      "Fits Avalanche models without factory cladding only. Use your trucks factory bulbs - 100% plug and play setup (Bulbs not included)",
   },
   sellingPoints: [
     "Crystal Clear Vision",
